cd7e1bad94e20de07d53157c452a7a6cd619d404
Jbla484 <jbla484@wgu.edu>
2023-10-18 19:08:10 -0700

    update socket on server

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
-        origin: `http://${ip}:3000`,
+        origin: `https://mern-webchat.onrender.com:3000`,
         methods: ['GET', 'POST'],
     },
 });
